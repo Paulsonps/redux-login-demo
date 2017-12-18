0a198853e613cbffd531636ea9711bec62701b0b
akhila <akhi.prabha@gmail.com>
2017-12-18 15:14:12 +0530

    Action Integration

---
+++ src/store/actions/actions.ts
@@ -0,0 +1,25 @@
+import { Action } from '@ngrx/store';
+
+
+export const LOAD_LOGIN = 'load login';
+export const LOAD_LOGIN_SUCESS = 'load login sucess';
+export const LOAD_LOGIN_FAIL = 'load login fail';
+
+export class LoadLogin implements Action {
+    readonly type = LOAD_LOGIN;
+    constructor(public payload?: any) { }
+ }
+ export class LoadLoginSucess implements Action {
+    readonly type = LOAD_LOGIN_SUCESS;
+    constructor(public payload?: any) { }
+ }
+
+ export class LoadLoginFail implements Action {
+    readonly type = LOAD_LOGIN_FAIL;
+    constructor(public payload?: any) { }
+ }
+
+// action type
+export type PropertyAction = | LoadLogin
+                             | LoadLoginSucess
+                             | LoadLoginFail;
